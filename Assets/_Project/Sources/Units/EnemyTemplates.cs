d670b105c0815a009dc6978a099cfc3d1498128c
Edward-Khaymanov <edwardkhaymanov@gmail.com>
2024-03-29 18:43:19 +0300

    add enemy types - templates

---
+++ Assets/_Project/Sources/Units/EnemyTemplates.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClubTest
+{
+    [CreateAssetMenu(menuName = "_Project/EnemyTemplates")]
+    public class EnemyTemplates : ScriptableObject
+    {
+        [field: SerializeField] public Dictionary<EnemyType, Enemy> Templates { get; private set; }
+    }
+}

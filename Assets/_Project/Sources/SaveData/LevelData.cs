eaefaaebdfcd8f8e53873aa10e72008cb733147d
Edward-Khaymanov <edwardkhaymanov@gmail.com>
2024-03-29 19:52:21 +0300

    add level data

---
+++ Assets/_Project/Sources/SaveData/LevelData.cs
@@ -0,0 +1,14 @@
+﻿using AYellowpaper.SerializedCollections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClubTest
+{
+    [CreateAssetMenu(menuName = "_Project/LevelData")]
+    public class LevelData : ScriptableObject
+    {
+        [field: SerializeField] public SerializedDictionary<EnemyType, int> EnemyTypeAmountToSpawn { get; private set; }
+        [field: SerializeField] public SerializedDictionary<EnemyType, List<DropedItem>> EnemyDropList { get; private set; }
+
+    }
+}

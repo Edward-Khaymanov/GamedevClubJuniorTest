211d1edec9709844d45127371e5b55fd13f7efd3
Edward-Khaymanov <edwardkhaymanov@gmail.com>
2024-03-29 19:49:55 +0300

    add helpers class

---
+++ Assets/_Project/Sources/Infrastructure/Helpers.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ClubTest
+{
+    public static class Helpers
+    {
+        public static Vector2 GetRandomPointBeetween(Vector2 min, Vector2 max)
+        {
+            return new Vector2(Random.Range(min.x, max.x), Random.Range(min.y, max.y));
+        }
+    }
+}

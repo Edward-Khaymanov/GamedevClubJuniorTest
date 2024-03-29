237701cf13a984df51e101b318a6942eda2ae0b8
Edward-Khaymanov <edwardkhaymanov@gmail.com>
2024-03-29 21:43:51 +0300

    add level data installer

---
+++ Assets/_Project/Sources/Installers/LevelDataInstaller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace ClubTest
+{
+    public class LevelDataInstaller : MonoInstaller
+    {
+        [SerializeField] private LevelData _levelData;
+
+        public override void InstallBindings()
+        {
+            Container
+                .Bind<LevelData>()
+                .FromInstance(_levelData);
+        }
+    }
+}
